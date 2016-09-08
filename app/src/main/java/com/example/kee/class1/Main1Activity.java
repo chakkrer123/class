8e2e515c8ccd4caa7cd9e0eef090f3e4a05b263b
Kee123 <docghwe31@gmail.com>
2016-09-08 11:51:18 +0700

    เสร็จแล้ว

---
+++ app/src/main/java/com/example/kee/class1/Main1Activity.java
@@ -0,0 +1,31 @@
+package com.example.kee.class1;
+
+import android.content.Intent;
+import android.content.res.ObbInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main1Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
---
+++ app/src/main/java/com/example/kee/class1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.kee.class1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button t1, t2;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1 = (Button) findViewById(R.id.button);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main1Activity.class);
+                startActivity(intent);
+            }
+        });
+        t2 = (Button) findViewById(R.id.button2);
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
